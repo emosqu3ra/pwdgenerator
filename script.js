65876e514858f5f7a51fe0a16312ea7ebccfa983
E L Ortiz <elortiz@Es-MacBook-Air.local>
2022-03-25 00:55:39 -0400

    worked on array and var for pwd generator

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Start your code ***HERE*** =========
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+const chars = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K','k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', 0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','%','^','&','*','(',')','-','+','='];
+
 
 // create a global variable called "pwLength" with a number between 10 and 18
 
+const pwLength = 11;
+
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
